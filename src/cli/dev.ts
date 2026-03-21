a91156953f96bce62a8b4ee8a923ae66c5765de4
Luis Angel F.R <luis@devlusoft.com>
2026-03-21 15:57:36 -0600

    feat: update version to 0.4.1-beta.2 and refactor dev server implementation

--- src/cli/dev.ts
+++ src/cli/dev.ts
@@ -1,9 +1,15 @@
-import { spawnSync } from "node:child_process";
+import { spawnSync } from 'node:child_process'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 while (true) {
-    const result = spawnSync(process.execPath, ['./devix-dev-server.js'], {
+    const result = spawnSync(process.execPath, [resolve(__dirname, 'dev-server.js')], {
         stdio: 'inherit',
         env: process.env,
     })
     if (result.status !== 75) break
 }
+
+export { }
